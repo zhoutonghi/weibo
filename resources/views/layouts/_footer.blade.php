87b16be07a59db4450419f6698f0b01d5602e3c4
zhoutong <zhoutonghi@hotmail.com>
2019-08-01 13:59:13 +0000

    切割头部和尾部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <img src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png" class="brand-icon" alt="">
+    <a href="#" target="_blank">
+        再看，再看就把你抓起来。
+    </a>
+    <div class="float-right">
+        <a href="/about">嘿嘿</a>
+    </div>
+</footer>
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,18 +5,13 @@
     <link rel="stylesheet" href="{{mix('css/app.css')}}">
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container">
-        <a href="/" class="navbar-brand">悬赏榜</a>
-        <ul class="navbar-nav justify-content-end">
-            <li class="nav-item"><a href="/help" class="nav-link">帮助</a></li>
-            <li class="nav-item"><a href="#" class="nav-link">登录</a></li>
-        </ul>
-    </div>
-</nav>
+@include('layouts._header')
 
 <div class="container">
-    @yield('content')
+    <div class="offset-md-1 col-md-10">
+        @yield('content')
+        @include('layouts._footer')
+    </div>
 </div>
 </body>
 </html>
