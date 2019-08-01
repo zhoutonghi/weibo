50c30d5cf1ac7a936d73bed83ad9bcd1a6518c5b
zhoutong <zhoutonghi@hotmail.com>
2019-08-01 13:34:46 +0000

    初始化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,8 +2,21 @@
 <html lang="en">
 <head>
     <title>@yield('title','weibo app') - Laravel 新手入门教程</title>
+    <link rel="stylesheet" href="{{mix('css/app.css')}}">
 </head>
 <body>
-@yield('content')
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <a href="/" class="navbar-brand">悬赏榜</a>
+        <ul class="navbar-nav justify-content-end">
+            <li class="nav-item"><a href="/help" class="nav-link">帮助</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">登录</a></li>
+        </ul>
+    </div>
+</nav>
+
+<div class="container">
+    @yield('content')
+</div>
 </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,18 @@
 @extends('layouts.default')
 @section('content')
-    <h1>主页</h1>
+    <div class="jumbotron">
+        <p class="lead">
+            你现在看到的是犯了严重罪行的罪恶人名单
+        </p>
+        <p>
+            他们分别是
+            <strong style="color: red; border: 2px dotted red;">刘锋</strong>
+            <strong style="color: green; border:  2px dotted green;">叶子</strong>
+            <strong style="color: royalblue; border:  2px dotted royalblue;">张奕圳</strong>
+        </p>
+        <p>如果遇到他们，请小心。</p>
+        <p>
+            <a href="#" class="btn btn-lg btn-success" role="button">现在举报</a>
+        </p>
+    </div>
 @stop
